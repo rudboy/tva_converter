bb352506682d4e40cbcfd4d248b64e9b222f6c95
RuddyMJ <rudboy972@gmail.com>
2019-02-07 17:30:59 +0100

    creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,128 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import taux from "./taux.json";
+
+const calcul = (montant, total) => {
+  return total - montant;
+};
 
 class App extends Component {
+  state = {
+    montant: "",
+    ttc: 0.0,
+    tva: "0.2",
+    totaltva: 0.0
+  };
+  handleChange = event => {
+    let result1 = 0;
+    let result2 = 0;
+    let total = 0;
+
+    const name = event.target.name;
+    const value = event.target.value;
+    const obj = {};
+    obj[name] = value;
+
+    // if (name === "montant") {
+    //   obj.ttc = this.state.montant * this.state.tva;
+    // }
+    if (name === "montant") {
+      this.setState(obj, () => {
+        result1 =
+          Number(this.state.montant) +
+          Number(this.state.montant) * Number(this.state.tva);
+        this.setState({
+          ttc: result1
+        });
+      });
+    }
+
+    if (name === "ttc") {
+      this.setState(obj, () => {
+        result2 =
+          Number(this.state.ttc) -
+          Number(this.state.ttc) * Number(this.state.tva);
+        this.setState({
+          montant: result2
+        });
+      });
+    }
+    if (name === "tva") {
+      this.setState(obj, () => {
+        result1 =
+          Number(this.state.montant) +
+          Number(this.state.montant) * Number(this.state.tva);
+        this.setState({
+          ttc: result1
+        });
+      });
+    }
+    this.setState(obj, () => {
+      total = console.log();
+    });
+  };
+
   render() {
+    const tauxTva = Object.keys(taux).map((valueKey, index) => (
+      <option key={index} value={taux[valueKey]}>
+        {valueKey}
+      </option>
+    ));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="contenair">
+          <div>
+            <text>Saisissez ci-dessous un montant hors taxes ou TTC :</text>
+          </div>
+          <section>
+            <div>
+              <p> Montant HT</p>
+              <input
+                placeholder="Exemple:50"
+                type="text"
+                name="montant"
+                id="montant"
+                value={this.state.montant}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <p> Montant TTC</p>
+              <input
+                placeholder="Exemple:50"
+                type="text"
+                name="ttc"
+                id="ttc"
+                value={this.state.ttc}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <p> Taux de TVA</p>
+              <select
+                placeholder="Exemple:50"
+                type="text"
+                name="tva"
+                id="tva"
+                value={this.state.tva}
+                onChange={this.handleChange}
+              >
+                {tauxTva}
+              </select>
+            </div>
+            <p> Montant de la TVA</p>
+            <div>
+              <input
+                placeholder="Exemple:50"
+                type="text"
+                name="totaltva"
+                id="totaltva"
+                value={this.state.ttc - this.state.montant}
+                onChange={this.handleChange}
+              />
+            </div>
+          </section>
         </header>
       </div>
     );
